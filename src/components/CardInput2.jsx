473c53096b8b80723b8568b5b64806fdb4e96c02
hong sipyo <155404215+SipYoHONG@users.noreply.github.com>
2024-03-28 12:38:46 +0900

    28일

--- src/components/CardInput2.jsx
+++ src/components/CardInput2.jsx
@@ -17,11 +17,11 @@ export default function CardInput2(props) {
             <h3>{form.email}</h3>
             <br />
             <form onSubmit={handleSubmit}>
-                <label>이름:</label>
+                <label htmlFor='name'>이름:</label>
                 <input type='text' id='name' name='name' value={form.name} 
                     onChange={handleChange}
                 /><br />
-                <label>이메일:</label>
+                <label htmlFor='email'>이메일:</label>
                 <input type='text' id='email' name='email' value={form.email} 
                     onChange={handleChange}
                 /><br />
